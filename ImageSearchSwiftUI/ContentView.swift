350cac5b7d1c453c3f8a1aa89b2991e0c339a194
YOO HANBYEOL <yhb4ever@gmail.com>
2020-10-12 16:19:19 +0900

    Initial Commit

---
+++ ImageSearchSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageSearchSwiftUI
+//
+//  Created by APPLE on 2020/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageSearchSwiftUI/ImageSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchSwiftUIApp.swift
+//  ImageSearchSwiftUI
+//
+//  Created by APPLE on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
